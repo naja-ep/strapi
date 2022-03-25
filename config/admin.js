67c1eee00b4b3f90d26c179494621d96a93e8043
naja-ep <naja@epart.com>
2022-03-25 16:32:21 +0900

    add admin language

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '3042d918fb1f9921b19297935436c10f'),
+    secret: env("ADMIN_JWT_SECRET", "3042d918fb1f9921b19297935436c10f"),
   },
+  url: "/dashboard",
 });
